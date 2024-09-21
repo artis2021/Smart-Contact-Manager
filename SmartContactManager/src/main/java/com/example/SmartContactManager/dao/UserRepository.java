9857092a385466b559d9aa8a103d6687b44e9a91
Arti Sahu <arti.sahu@firsthive.com>
2024-09-21 21:46:21 +0530

    Added Spring security.

--- SmartContactManager/src/main/java/com/example/SmartContactManager/dao/UserRepository.java
+++ SmartContactManager/src/main/java/com/example/SmartContactManager/dao/UserRepository.java
@@ -8,6 +8,4 @@ import org.springframework.data.repository.query.Param;
 public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("select u from User u where u.email = :email")
     public User getUserByUserName(@Param ("email") String email);
-
-
 }
