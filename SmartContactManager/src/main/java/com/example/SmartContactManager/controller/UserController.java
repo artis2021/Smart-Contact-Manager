f72768708e8151ebb569756e399fb595b6208447
Arti Sahu <arti.sahu@firsthive.com>
2024-09-22 18:19:01 +0530

    Designing add contact form.

--- SmartContactManager/src/main/java/com/example/SmartContactManager/controller/UserController.java
+++ SmartContactManager/src/main/java/com/example/SmartContactManager/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.SmartContactManager.controller;
 
 import com.example.SmartContactManager.dao.UserRepository;
+import com.example.SmartContactManager.entities.Contact;
 import com.example.SmartContactManager.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -16,9 +19,8 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/index")
-    public String dashBoard(Model model, Principal principal){
-
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal){
         String username = principal.getName();
         System.out.println("Username: "+username);
 
@@ -26,7 +28,20 @@ public class UserController {
         System.out.println("USER: "+ user);
 
         model.addAttribute("user", user);
-//        return "about";
+    }
+
+
+    @RequestMapping("/index")
+    public String dashBoard(Model model, Principal principal){
+        model.addAttribute("title", "User Dashboard");
         return "normal/user_dashboard";
+    }
+
+    //add contact handler
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model){
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
     }
 }
